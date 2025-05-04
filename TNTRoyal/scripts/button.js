3636dd6f23013d7a218e5b19aa7b8563db2ff411
altivelis <ayupon89821026@gmail.com>
2025-05-04 10:33:07 +0900

    観戦ボタン追加

--- TNTRoyal/scripts/button.js
+++ TNTRoyal/scripts/button.js
@@ -2,13 +2,27 @@ import * as mc from "@minecraft/server";
 import { startGame } from "./main";
 
 mc.world.beforeEvents.playerInteractWithBlock.subscribe(data=>{
-  let {isFirstEvent, block} = data;
+  let {isFirstEvent, block, player} = data;
   if(!data.isFirstEvent) return;
   if(`${block.location.x} ${block.location.y} ${block.location.z}` == "-1 -58 8") {
     //スタートボタン
     data.cancel = true;
     mc.system.run(()=> {
       startGame();
+    })
+  }
+  if(`${block.location.x} ${block.location.y} ${block.location.z}` == "1 -58 8") {
+    //観戦ボタン
+    data.cancel = true;
+    mc.system.run(()=> {
+      if(player.hasTag("spectator")) {
+        player.removeTag("spectator");
+        player.sendMessage("§a観戦モードを解除しました。");
+      }
+      else {
+        player.addTag("spectator");
+        player.sendMessage("§a観戦モードになりました。");
+      }
     })
   }
 })
