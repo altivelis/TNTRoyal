bdbd8fe2a7bb958524def002da9b5880ef139ce5
altivelis <ayupon89821026@gmail.com>
2025-05-08 22:43:16 +0900

    能力(試験的)作成

---
+++ TNTRoyal/scripts/role.js
@@ -0,0 +1,31 @@
+import * as mc from "@minecraft/server";
+
+export const roleList = [
+  {
+    name: "ノーマル",
+    initBomb: 1,
+    maxBomb: 8,
+    initPower: 2,
+    maxPower: 8,
+    initSpeed: 0,
+    maxSpeed: 8,
+  },
+  {
+    name: "ウマ",
+    initBomb: 1,
+    maxBomb: 8,
+    initPower: 1,
+    maxPower: 5,
+    initSpeed: 6,
+    maxSpeed: 12
+  },
+  {
+    name: "クリーパー",
+    initBomb: 1,
+    maxBomb: 6,
+    initPower: 8,
+    maxPower: 16,
+    initSpeed: 0,
+    maxSpeed: 4
+  }
+]
