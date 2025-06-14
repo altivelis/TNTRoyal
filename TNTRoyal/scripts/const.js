46877d13d29c3fe98839e700698ffd3104beb8ab
altivelis <ayupon89821026@gmail.com>
2025-06-14 23:57:06 +0900

    ネザー、廃坑ステージ追加

--- TNTRoyal/scripts/const.js
+++ TNTRoyal/scripts/const.js
@@ -9,6 +9,7 @@ export const roby = {x:0.5, y:-58.5, z:0.5};
  * @property {Array<mc.Vector3>} spawn - The spawn points within the stage.
  * @property {mc.Vector3} pivot - The pivot point of the stage.
  * @property {string} block - The block type used in the stage.
+ * @property {string} icon - An optional icon for the stage.
  */
 export const stage = [
   {
@@ -19,6 +20,7 @@ export const stage = [
     ],
     pivot: {x:-7, y:-60, z:-70},
     block: "minecraft:brick_block",
+    icon: "textures/blocks/planks_oak.png"
   },
   {
     name: "snowy_taiga",
@@ -27,7 +29,8 @@ export const stage = [
       {x:-70, y:-59, z:-69}, {x:-58, y:-59, z:-59}, {x:-70, y:-59, z:-59}, {x:-58, y:-59, z:-69}
     ],
     pivot: {x:-71, y:-60, z:-70},
-    block: "minecraft:snow"
+    block: "minecraft:snow",
+    icon: "textures/blocks/planks_spruce.png"
   },
   {
     name: "desert",
@@ -36,7 +39,28 @@ export const stage = [
       {x:58, y:-59, z:-69}, {x:70, y:-59, z:-59}, {x:58, y:-59, z:-59}, {x:70, y:-59, z:-69}
     ],
     pivot: {x:57, y:-60, z:-70},
-    block: "minecraft:azalea_leaves"
+    block: "minecraft:azalea_leaves",
+    icon: "textures/blocks/sand.png"
+  },
+  {
+    name: "nether_fortress",
+    area: {start: {x:-70, y:-59, z:-5}, end: {x:-58, y:-59, z:5}},
+    spawn: [
+      {x:-70, y:-59, z:-5}, {x:-58, y:-59, z:5}, {x:-70, y:-59, z:5}, {x:-58, y:-59, z:-5}
+    ],
+    pivot: {x:-71, y:-62, z:-6},
+    block: "minecraft:netherrack",
+    icon: "textures/blocks/nether_brick"
+  },
+  {
+    name: "mineshaft",
+    area: {start: {x:58, y:-59, z:-5}, end: {x:70, y:-57, z: 5}},
+    spawn: [
+      {x:58, y:-59, z:-5}, {x:70, y:-59, z:5}, {x:58, y:-59, z:5}, {x:70, y:-59, z:-5}
+    ],
+    pivot: {x:57, y:-60, z:-6},
+    block: "minecraft:cobblestone",
+    icon: "textures/blocks/cobblestone.png"
   }
 ]
 
@@ -45,6 +69,7 @@ export const breakable_block = [
   "minecraft:brick_block",
   "minecraft:snow",
   "minecraft:azalea_leaves",
+  "minecraft:netherrack",
   "minecraft:cobblestone",
 ]
 
